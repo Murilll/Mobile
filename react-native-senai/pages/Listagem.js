8f00318c384ce34dc17b80ed3f0ea99bf397892e
Murilll <murilosertorio25@gmail.com>
2023-06-27 17:12:45 -0300

    aaaa

--- react-native-senai/pages/Listagem.js
+++ react-native-senai/pages/Listagem.js
@@ -1,12 +1,39 @@
 import { Text, View } from "react-native";
+import axios from 'axios';
 
 import styles from "../styles/styles";
+import { useEffect, useState } from "react";
 
 export default function Listagem() {
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(() => {
+    const getTransactions = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/transactions')
+        console.log(response)
+        if (response.status === 200) {
+          setTransactions(response.data)
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getTransactions()
+  }, [])
 
   return (
-    <View style={styles.container}>
-      <Text>Listagem</Text>
+    <View>
+      <Text></Text>
+        { transactions.map((transaction, index) => {
+          return (
+            <View>
+              <Text>{ transaction.reference }</Text>
+              <Text>{ transaction.amount }</Text>
+              <Text>{ transaction.date }</Text>
+            </View>
+          )
+        }) }
     </View>
   );
 }
